681d87d89dad46698a68f9a339fd272dbaa3ed65
Sarif27 <203125295+Sarif27@users.noreply.github.com>
2025-07-13 19:36:12 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './componen/Navigation';
+function App() {
+  return (
+    <Navigation />
+  );
+}
+
+export default App;
